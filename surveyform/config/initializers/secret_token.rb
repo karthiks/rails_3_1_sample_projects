516ec8cc2a04e691cef1779c07ac7a63ffcc4a34
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-11-05 09:05:06 +0530

    New project surveyforms - to learn nested associations and stuff

---
+++ surveyform/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveyform::Application.config.secret_token = '349f3364c6e142627d6f131891496f1a7413c2c4c22d2d61205a108796f720c8e3c96970e9339fba6fe67351232789cda3b1eb2f6dedde6bb58081cb22a092e4'
