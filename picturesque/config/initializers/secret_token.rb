952de30d26c874fe51da6bf5dbf21e51582c89fa
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-11-09 20:45:58 +0530

    New Project to try out CarrierWave gem for File Uploads | Initial commit

---
+++ picturesque/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picturesque::Application.config.secret_token = '8e9475cab17054a1522cf11628820766e09aa6b4b9cee6f1738bfada8f1cd34ed246eb4a4c152f024b7d2532a01052e3df9e7aeea1359d7465b30fa35e962627'
