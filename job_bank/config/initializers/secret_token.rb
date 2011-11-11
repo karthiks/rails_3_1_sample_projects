d7e33bdb72e22f049110a826ee43f0b2aa648d0f
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-11-11 22:31:34 +0530

    Created new project to experiment with attachments using carrierwave gem

---
+++ job_bank/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBank::Application.config.secret_token = 'e7c8e8c5a1b1c5bb50b04565ff5e4dd7aacd06f4c16e3912ccf24a7d9e94ef4be84989c3ff4a04a3fb01c78c4ab7d4f86fe754576469de9fa1682e7c5fa4ab4e'
