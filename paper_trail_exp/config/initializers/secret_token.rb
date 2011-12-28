80850d7dc9727e3a73aa2a177ca034bce9808746
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-12-28 07:15:47 +0530

    Created new project to experiment with paper_trail

---
+++ paper_trail_exp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperTrailExp::Application.config.secret_token = 'acd93ab47b5a08bd81adfa3160e38b2701997555fb2f555305aebad03729413d633316a1016203c70c48c6b70906ea89682b16705f92fb63955b8a8090e75418'
